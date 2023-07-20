2ae9de3ca1037e21d596fb2fcdfe8453025b2151
sooryasrajan <soorya.s27@gmail.com>
2023-07-20 23:03:00 +0530

    Added theme colors material

---
+++ src/main/frontend/src/themes/theme-schema.tsx
@@ -0,0 +1,32 @@
+export interface ThemeSchema {
+    primary: string;
+    onPrimary: string;
+    primaryContainer: string;
+    onPrimaryContainer: string;
+    secondary: string;
+    onSecondary: string;
+    secondaryContainer: string;
+    onSecondaryContainer: string;
+    tertiary: string;
+    onTertiary: string;
+    tertiaryContainer: string;
+    onTertiaryContainer: string;
+    error: string;
+    errorContainer: string;
+    onError: string;
+    onErrorContainer: string;
+    background: string;
+    onBackground: string;
+    surface: string;
+    onSurface: string;
+    surfaceVariant: string;
+    onSurfaceVariant: string;
+    outline: string;
+    onInverseSurface: string;
+    inverseSurface: string;
+    inversePrimary: string;
+    shadow: string;
+    surfaceTint: string;
+    outlineVariant: string;
+    scrim: string;
+}
