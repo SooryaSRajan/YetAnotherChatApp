1e3a83e9782921007825b222f7d05040ef493a22
sooryasrajan <soorya.s27@gmail.com>
2023-07-21 19:22:10 +0530

    Added theme builder

--- src/main/frontend/src/themes/theme-schema.tsx
+++ src/main/frontend/src/themes/theme-schema.tsx
@@ -1,32 +1,19 @@
 export interface ThemeSchema {
     primary: string;
     onPrimary: string;
-    primaryContainer: string;
-    onPrimaryContainer: string;
     secondary: string;
     onSecondary: string;
-    secondaryContainer: string;
-    onSecondaryContainer: string;
-    tertiary: string;
-    onTertiary: string;
-    tertiaryContainer: string;
-    onTertiaryContainer: string;
     error: string;
-    errorContainer: string;
     onError: string;
-    onErrorContainer: string;
     background: string;
     onBackground: string;
     surface: string;
     onSurface: string;
-    surfaceVariant: string;
-    onSurfaceVariant: string;
-    outline: string;
-    onInverseSurface: string;
-    inverseSurface: string;
-    inversePrimary: string;
-    shadow: string;
-    surfaceTint: string;
-    outlineVariant: string;
-    scrim: string;
 }
+
+export enum ThemeType {
+    LIGHT = 'light',
+    DARK = 'dark',
+}
+
+export type ThemeMode = ThemeType;
