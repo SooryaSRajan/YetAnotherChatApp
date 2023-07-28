1f0d998cc0c652def6b391ceefd41119511bfd89
sooryasrajan <soorya.s27@gmail.com>
2023-07-28 23:53:50 +0530

    Removed queue usage

--- src/main/java/com/ssrprojects/ultimatechatapp/service/QueueService/QueueConsumer.java
+++ src/main/java/com/ssrprojects/ultimatechatapp/service/QueueService/QueueConsumer.java
@@ -1,13 +1,10 @@
 package com.ssrprojects.ultimatechatapp.service.QueueService;
 
 import com.ssrprojects.ultimatechatapp.service.QueueService.queue.QueueTask;
-import com.ssrprojects.ultimatechatapp.service.QueueService.queue.Task;
 import com.ssrprojects.ultimatechatapp.service.UserService.UserService;
 import com.ssrprojects.ultimatechatapp.utils.Constants;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
 
 @Component
 public class QueueConsumer {
@@ -20,17 +17,6 @@ public class QueueConsumer {
 
     @RabbitListener(queues = Constants.QUEUE_NAME)
     public void consumeMessageFromQueue(QueueTask queueTask) {
-        System.out.println("Message Received from queue: " + queueTask);
-
-        if (queueTask.getTask() == Task.REMOVE_UNVERIFIED_USER) {
-            System.out.println("Removing unverified user");
-            HashMap<String, String> properties = queueTask.getProperties();
-            System.out.println("User id: " + properties.get("userId"));
-
-            userService.removeUnverifiedUser(properties.get("userId"));
-        } else {
-            System.out.println("Unknown task");
-        }
 
     }
 }
--- src/main/java/com/ssrprojects/ultimatechatapp/service/QueueService/queue/Task.java
+++ src/main/java/com/ssrprojects/ultimatechatapp/service/QueueService/queue/Task.java
@@ -1,5 +1,5 @@
 package com.ssrprojects.ultimatechatapp.service.QueueService.queue;
 
 public enum Task {
-    REMOVE_UNVERIFIED_USER
+
 }
